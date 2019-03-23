fdeea4ae9cb44afe888faed40af4e721c18a8ada
Ajit Singh <ajit.singh3@mindtree.com>
2019-03-23 17:01:35 +0530

    Added CLI front-end and Bucketed Class

---
+++ bin/index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const chalk = require('chalk');
+
+const Bucketed = require('../lib/Bucketed');
+
+const packageJson = require('../package.json');
+
+const log = {
+    info: (msg) => console.log(chalk.bold.green(msg) + "\n"),
+    error: (msg) => console.log(chalk.bold.red(msg) + "\n"),
+    warn: (msg) => console.log(chalk.keyword('orange')(msg) + "\n")
+};
+
+program.version(`${packageJson.name}: v${packageJson.version}`, '-v, --version')
+    .description(packageJson.description);
+
+program
+    .command('deploy')
+    .alias('d')
+    .description('Deploy your static site content to specified bucket.')
+    .action(deploy);
+
+// Assert that a VALID command is provided 
+if (!process.argv.slice(2).length) {
+    log.error("Error ! No command specified. Please have a look at help section")
+    program.outputHelp();
+    process.exit();
+}
+
+program.parse(process.argv);
+
+function deploy() {
+    try {
+        let bucketed = new Bucketed();
+        log.info(JSON.stringify(bucketed.deploy()));
+    } catch (error) {
+        log.error(error);
+    }
+
+}
